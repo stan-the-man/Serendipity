2077b5c076e10a0331e4444552f84033043b2a48
Stan Whitcomb <stanwhitcomb@outlook.com>
2015-04-07 14:33:29 -0700

    Puttin shit in other shit

---
+++ gitPush.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Pushing to github ... "
+COMMENT="Puttin shit in other shit"
+
+git add .
+git commit -m "$COMMENT"
+git remote add origin https://github.com/stan-the-man/Serendipity.git
+git remote -v
+git push origin master
+
+
+echo "Finished adding!" 
